c47c9cd9101ee280af625709d9e8ce208f021b63
Santiago Villa <code@santiagovilla.it>
2025-05-03 17:03:11 +0200

    formatted styling for PostCard element

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,29 +1,28 @@
 import PostCard from "../components/PostCard";
 import { useEffect, useState } from "react";
 
-function HomePage (){
-    const [posts, setPosts] = useState([]);
+function HomePage() {
+  const [posts, setPosts] = useState([]);
 
-    useEffect(() => {
-        fetch("http://localhost:3000/posts")
-        .then ((res) => res.json())
-        .then ((data) => setPosts(data))
-        .catch((err) => console.error("Error fetching posts:", err));
-    }, []);
+  useEffect(() => {
+    fetch("http://localhost:3000/posts")
+      .then((res) => res.json())
+      .then((data) => setPosts(data))
+      .catch((err) => console.error("Error fetching posts:", err));
+  }, []);
 
-
-    return (
-        <div>
-            <h1 className="text-2xl font-bold mb-4">Posts</h1>
-            <ul className="space-y-4">
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <PostCard post={post} />
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Posts</h1>
+      <ul className="space-y-4">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <PostCard post={post} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default HomePage;
