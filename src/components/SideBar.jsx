5a4d98b68c0b80e451acbc9aa5704847ed5553b7
Santiago Villa <code@santiagovilla.it>
2025-05-16 22:12:18 +0200

    added logo

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,37 +1,39 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 
+function SideBar() {
+  return (
+    <div className="w-64 bg-slate-900 text-white p-6 flex flex-col space-between h-screen">
+      <div className="mb-4">
+        <img
+          src={logo}
+          alt="Site Logo"
+          className="h-25 w-auto object-contain mx-auto"
+        />
+      </div>
+      <hr className="my-6 border-gray-600" />
+      <div>
+        <nav className="flex flex-col">
+          <Link to="/">Posts</Link>
+          <Link to="/create">Crear Post</Link>
+        </nav>
+      </div>
 
-function SideBar () {
-    return (
-        <div className="w-64 bg-gray-800 text-white p-6 flex flex-col space-between h-screen">
-            <h1 className="text-2xl font-bold mb-4">Logo</h1>
-            <div>
-            <nav className="flex flex-col">
-                <Link to = "/">Posts</Link>
-                <Link to = "/create">Crear Post</Link>
-            </nav>
-            </div>
+      <hr className="my-6 border-gray-600" />
 
-            <hr className="my-6 border-gray-600" />
+      <nav className="flex flex-col text-gray-400">
+        <Link to="/rules">Normas de la comunidad</Link>
+        <Link to="/themes">Otros temas</Link>
+      </nav>
 
-              <nav className="flex flex-col text-gray-400">
-                <Link to = "/rules">Normas de la comunidad</Link>
-                <Link to = "/themes">Otros temas</Link>
-            </nav>
+      <hr className="my-6 border-gray-600" />
 
-            <hr className="my-6 border-gray-600" />
-
-            <nav className="flex flex-col text-gray-400">
-                <Link to = "/about">Información</Link>
-                <Link to = "/contact">Contacto</Link>
-            </nav>
-
-            
-
-          
-
-        </div>
-    );
+      <nav className="flex flex-col text-gray-400">
+        <Link to="/about">Información</Link>
+        <Link to="/contact">Contacto</Link>
+      </nav>
+    </div>
+  );
 }
 
-export default SideBar
+export default SideBar;
